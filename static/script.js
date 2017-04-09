d9b9432f13b42315bae46d09ffb32eb44d85ba27
Rahul Sondhi <Rahul Sondhi>
2017-04-09 00:13:02 -0400

    completly redid starting and redoing rest

--- static/script.js
+++ static/script.js
@@ -1,16 +1,10 @@
 /*Login Functions and Var*/
 // Get the modal
 var modal = document.getElementById('id01');
-var modal2 = document.getElementById('id02');
-var modal3 = document.getElementById('id03');
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
-        modal.style.display = "none";
-    }else if (event.target == modal2) {
-        modal.style.display = "none";
-    }else {
         modal.style.display = "none";
     }
 }
