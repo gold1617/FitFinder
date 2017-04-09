cda8506ef7d1196ccb2976e97b24d45c6728a295
Rahul Sondhi <Rahul Sondhi>
2017-04-09 03:39:50 -0400

    added data taking function

--- static/script.js
+++ static/script.js
@@ -10,5 +10,19 @@ window.onclick = function(event) {
 }
 
 function sendData (){
-  
+
+}
+
+function getData(){
+  $.get("/myprofile",function(data, status){
+    document.getElementByName("age").value=(data.age);
+    document.getElementByName("height").value=(data.height);
+    document.getElementByName("name").value=(data.name);
+    document.getElementByName("bio").value=(data.bio);
+  });
+}
+
+function accountInfo(){
+  getData();
+  document.getElementById('id02').style.display='block';
 }
